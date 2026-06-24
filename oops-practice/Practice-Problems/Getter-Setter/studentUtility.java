6ffdc6a2cd054a680aeab160086873af5896e66c
Surbhi Singh <surbhisingh1258@gmail.com>
2026-06-24 20:22:29 +0530

    Added oops-practice folder

--- oops-practice/Practice-Problems/Getter-Setter/studentUtility.java
+++
@@ -1,9 +0,0 @@
-public class studentUtility{
-    public static void main(String[] args){
-        Student s = new Student();
-
-        s.setName("surbhi");
-        System.out.println(s.getName());
-        System.out.println(s.getID());
-    }
-}
